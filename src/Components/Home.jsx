4f49c4f81a14d2e575d80d1eb243b03446e1df3c
Fethedddine <91231544+Fetheddine-Addad@users.noreply.github.com>
2024-01-01 19:19:32 +0000

    image changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Data_Cloud.jpg";
+import image from "../images/CloudData.jpg";
 
 const imageAltText = "A man staring at a screen with a Cloud network And some illustration of data usage in our era";
 
