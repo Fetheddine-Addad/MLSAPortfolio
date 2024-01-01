0f74a7d13bdc29c27efa2277baa71c90a67fe90e
Fethedddine <91231544+Fetheddine-Addad@users.noreply.github.com>
2024-01-01 18:00:46 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Fetheddine Addad",
+  title: "Cloud and Data Enthusiast",
+  email: "addadfetheddine@gmail.com",
+  gitHub: "https://github.com/Fetheddine-Addad",
+  instagram: "https://www.instagram.com/fetheddineaddad/",
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
